e8f589c5c6aa01f77525d3ac55b9b0c5577dd6ad
samullman <samullman@gmail.com>
2016-12-15 02:42:22 -0800

    sign up form

--- app/assets/javascripts/components/Auth.jsx
+++ app/assets/javascripts/components/Auth.jsx
@@ -44,8 +44,33 @@ const SignUp = React.createClass({
       "color" : "#262262" ,
     }
 
+    var businessCodes = [
+      "11: Agriculture, Forestry, Fishing and Hunting" ,
+      "21: Mining, Quarrying, and Oil and Gas Extraction" ,
+      "22: Utilities" ,
+      "23: Contruction" ,
+      "31-33: Manufacturing" ,
+      "42: Wholesale Trade" ,
+      "44-45: Retail Trade" ,
+      "48-49: Transportation and Warehousing" ,
+      "51: Information" ,
+      "52: Finance and Insurance" ,
+      "53: Real Estate and Rental and Leasing" ,
+      "54: Professional, Scientific, and Technical Services" ,
+      "55: Management of Companies and Enterprises" ,
+      "56: Administrative and Support and Waste Management and Remediation Services" ,
+      "61: Educational Services" ,
+      "62: Health Care and Social Assistance" ,
+      "71: Arts, Entertainment, and Recreation" ,
+      "72: Accommodation and Food Services" ,
+      "81: Other Services (except Public Administration)" ,
+      "92: Public Administration" ,
+
+    ]
+
     return({
-      style: style,
+      style : style ,
+      businessCodes : businessCodes ,
     })
   },
 
@@ -68,6 +93,100 @@ const SignUp = React.createClass({
             <label>Password</label>
             <input type="password" required />
           </div>
+
+          <div className="field">
+            <label>Confirm Password</label>
+            <input type="password" required />
+          </div>
+
+          <div className="two fields">
+            <div className="field">
+              <label>First Name</label>
+              <input type="text" required />
+            </div>
+
+            <div className="field">
+              <label>Last Name</label>
+              <input type="text" required />
+            </div>
+          </div>
+
+          <div className="field">
+            <label>Company Name</label>
+            <input type="text" />
+          </div>
+
+          <div className="field">
+            <label>Mailing Address</label>
+            <input type="text" />
+          </div>
+
+          <div className="three fields">
+            <div className="field">
+              <label>City</label>
+              <input type="text" />
+            </div>
+
+            <div className="field">
+              <label>State</label>
+              <input type="text" />
+            </div>
+
+            <div className="field">
+              <label>Zip</label>
+              <input type="text" />
+            </div>
+          </div>
+
+          <div className="field">
+            <label>Country</label>
+            <input type="text" />
+          </div>
+
+          <div className="field">
+            <label>Physical Address</label>
+            <input type="text" />
+          </div>
+
+          <div className="three fields">
+            <div className="field">
+              <label>City</label>
+              <input type="text" />
+            </div>
+
+            <div className="field">
+              <label>State</label>
+              <input type="text" />
+            </div>
+
+            <div className="field">
+              <label>Zip</label>
+              <input type="text" />
+            </div>
+          </div>
+
+          <div className="field">
+            <label>Country</label>
+            <input type="text" />
+          </div>
+
+          <div className="field">
+            <label>Business Type (NAICS Code)</label>
+            <select className="ui dropdown">
+              <option></option>
+              {
+                this.state.businessCodes.map( function (el) {
+                  return (
+                    <option value = { el } >
+                      { el }
+                    </option>
+                  )
+                })
+              }
+            </select>
+          </div>
+
+
         </div>
 
         <div className="ui button" style={{ "background" : "#262262" , "color" : "white" , "marginTop" : "24px" }} onClick={ this.signIn } >
