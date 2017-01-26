4bebd05b924f0d0904d92536ae0ea43cf67f7db9
samullman <samullman@gmail.com>
2017-01-26 12:54:22 -0800

    get rid of add sponsor button

--- app/assets/javascripts/components/AdminMailings.jsx
+++ app/assets/javascripts/components/AdminMailings.jsx
@@ -77,77 +77,9 @@ const AdminMailings = React.createClass({
             }
           </tbody>
 
-          <tfoot>
-            <tr>
-              <th colSpan="6">
-                <div className="ui button blue" onClick={ this.makeAd }>
-                  New Sponsor
-                </div>
-              </th>
-            </tr>
-          </tfoot>
+    
         </table>
       </div>
-    );
-  }
-});
-
-
-const AdminAdsRow = React.createClass({
-
-  delete (e) {
-    var sure = confirm('Are you sure?');
-
-    if (sure) {
-      $.ajax({
-        url: '/api/sponsors/' + e.currentTarget.dataset.id ,
-        type: 'DELETE' ,
-        success: function( app_data , resp , obj ) {
-          this.props.parent.setState(app_data)
-          this.props.mom.setState({
-            ads: app_data.ads
-          })
-        }.bind(this), error: function ( app_data , resp , obj ) {
-          alert('There was an erorr.')
-        }.bind(this)
-      })
-
-    }
-  },
-
-  edit () {
-    Backbone.history.navigate( 'edit/sponsor/' + this.props.ad.id , { trigger : true } )
-  },
-
-  render() {
-
-    return (
-      <tr>
-
-        <td >
-          { this.props.ad.name }
-        </td>
-
-        <td>
-          <img className="ui image small" src={ this.props.ad.image_url } alt={ this.props.ad.name }/>
-
-        </td>
-
-        <td >
-          { this.props.ad.link }
-        </td>
-
-        <td className="collapsing">
-
-          <a data-id={this.props.ad.id} onClick={ this.edit } style={{ "marginRight" : "8px" ,}}>
-            edit
-          </a>
-
-          <a data-id={this.props.ad.id} onClick={ this.delete }>
-            delete
-          </a>
-        </td>
-      </tr>
     );
   }
 });
