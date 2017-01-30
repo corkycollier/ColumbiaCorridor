74e9c5900d116b37d1762c17852b8b78126509dc
samullman <samullman@gmail.com>
2017-01-29 21:17:05 -0800

    member only directory

--- app/assets/javascripts/components/MemberOnlyDirectory.jsx
+++ app/assets/javascripts/components/MemberOnlyDirectory.jsx
@@ -12,32 +12,24 @@ const MemberOnlyDirectory = React.createClass({
           "padding" : "30px 20px" ,
         }}>
 
-        <h1>
-          Members
+        <h1 style={{ "marginBottom" : "36px" , }}>
+          Member Directory
         </h1>
 
-        <div className="member-mods">
+        <div className="member-mods" style={{ "minHeight" : "200px" , }}>
           {
             this.props.parent.state.members.map(function(el) {
               if (el.private) {
                 return
               } else {
                 return(
-                  <div className="member-mod" key={ "mod" + el.id } style={{
-                      "textAlign" : "center" ,
-                      "padding" : "4vw" ,
-                      "paddingTop" : "2vw" ,
-                    }}>
-
-                    <div style={{ "marginBottom" : "8px" , }}>
-                      <a href={ el.company_website } target="blank" style={{ "color" : "#262262" , }}>
-                        <b>
-                          <i>
-                            { el.company_name }
-                          </i>
-                        </b>
-                      </a>
-                    </div>
+                  <div style={{
+                      "fontSize" : "22px" ,
+                      "margin" : "40px 0px" ,
+                    }} key={ "mod" + el.id }>
+                    <a href={ el.company_website } target="blank" style={{ "color" : "#262262" , }}>
+                      { el.company_name }
+                    </a>
                   </div>
                 )
               }
