82617af40cf6b80a7e489277f1d3c908eea32a72
samullman <samullman@gmail.com>
2017-02-14 22:42:27 -0800

    re-aligned resources

--- app/assets/javascripts/components/pages/Resources.jsx
+++ app/assets/javascripts/components/pages/Resources.jsx
@@ -91,152 +91,152 @@ const Groundwater = React.createClass({
 
           <div className="two wide column">
             <img src="http://res.cloudinary.com/djjldnjz7/image/upload/v1487140042/groundwater_logo_fx3ycc.jpg"></img>
-              <a href="http://www.portlandoregon.gov/water/29890" target="_blank" >
-                <div className="ui fluid button" style={{ "fontWeight" : "bold" , "color" : "#262262" , "marginTop" : "12px" , "width" : "146px" }} >
-                  Water Bureau Site
-                </div>
-              </a>
+            <a href="http://www.portlandoregon.gov/water/29890" target="_blank" >
+              <div className="ui fluid button" style={{ "fontWeight" : "bold" , "color" : "#262262" , "marginTop" : "12px" , "width" : "146px" }} >
+                Water Bureau Site
+              </div>
+            </a>
           </div>
         </div>
+        
 
 
 
 
 
+      </div>
+    )
+  }
+});
 
+const ResourceFullUse = React.createClass({
+
+  updateState(e) {
+    var state = this.state;
+    state[ e.currentTarget.dataset.field ] = e.currentTarget.value
+    this.setState( state );
+  },
+
+  render () {
+    return (
+
+      <div className="ui container" style={{ "color" : "#262262" , "padding" : "30px 20px" ,}}>
+        <h1 className="ui header" style={{ "color" : "#262262"}}>
+          <a href="https://www.facebook.com/groups/ResourceFullUse/" target="_blank" style={{ "color" : "#262262" , }}>
+            ResourceFULL Use
+          </a>
+        </h1>
+
+        <div className="ui grid doubling stackable">
+          <div className="sixteen wide column" >
+            <p>
+              At a 2006 CCA breakfast forum, ResourceFULL Use was born.
+              We never imagined it would be going strong a decade later. Each exchange is money saved, resources utilized, and greenhouse gasses reduced. Our quarterly workshops are a great place to brainstorm and Dzspeed datedz with potential partners.
+            </p>
+
+            <h2>One company’s waste is another’s raw material</h2>
+
+            <p>
+              Industries and manufactures commonly produce wastes that other businesses could use. ResourceFull Use was started to help turn one business’ trash into another’s treasure.
+            </p>
+
+            <p>
+              ResourceFull Use provides manufacturers and industry with workshops to assess resource needs and actively exchange resources (e.g.. use the waste of one organization as an input for another).
+            </p>
+
+            <p>
+              We’ve also teamed with the Materials Exchange Platform to give members expanded capabilities and a broader audience for your by-products. Any materials that you post are now seen by businesses in Oregon and Washington.
+            </p>
+
+            <p>
+              Benefits are improved economic viability through reduced waste, improved resource efficiences, and decreased emissions (CO2 and other greenhouse gases).
+            </p>
+
+            <a href="https://www.facebook.com/groups/ResourceFullUse/" target="_blank" style={{ "color" : "#262262" , }}>
+
+              <img className="ui image large" src="http://res.cloudinary.com/djjldnjz7/image/upload/v1481692448/RESOURCES_RFU_1_03_sgwtw2.jpg" style={{ "float" : "left" , "margin" : "12px" }} / >
+                <img className="ui image large" src="http://res.cloudinary.com/djjldnjz7/image/upload/v1481692472/RESOURCES_RFU_1_07_zlpojo.jpg" style={{ "float" : "left" , "margin" : "12px" }} />
+              </a>
+            </div>
+
+          </div>
+
+          <div className="ui grid">
+            <div className="sixteen wide column">
+              The ResourceFull Use program is different from traditional passive web-based exchanges in these important ways:
+              <ul style={{"marginBottom":"24px"}}>
+                <li>It is actively facilitated. Whereas passive exchange databases
+                  rely on the user to identify exchanges, ResourceFull Use does
+                  not rely only on a passive database. Facilitated workshops
+                  and convened business association events provide venues for
+                  organization to actively explore potential exchanges.
+                </li>
+                <li>
+                  It looks holistically at all resource flows.
+                  Participating organizations map their resource inputs and outputs
+                  through the whole organization rather than just focusing on
+                  individual waste streams or input needs.
+                </li>
+                <li>
+                  It leverages local business associations and government programs. The exchange uses existing programs and relationships to
+                  build awareness and expand the use. It is local. It reduces
+                  transportation costs and promotes community.
+                </li>
+                <li>
+                  It is flexible. The program encompasses more than material
+                  resources. The process allows for exchanges involving space,
+                  equipment, energy, and transportation.
+                </li>
+              </ul>
+
+              <p>
+                The ResourceFull Use Project started in 2006 as the result of collaboration between Zero WasteAlliance, a Portland nonprofit organization and Columbia Corridor Association. In its first phase the ResourceFull Use program found a home for 250 gallons of paint and reduced 3 tons of greenhouse gasses.
+              </p>
+
+              <p>
+                The program has benefited from advisory, funding, and in-kind support from a wide cross- section of organizations including Pacific Northwest Pollution Prevention Resource Center, The Boeing Foundation, Cadmus Group (formely Quantec, LLC). Oregon Department of Environmental Quality (DEQ), Portland Development Commission (PDC), City of Portland’s Recycle at Work Program, Portland General Electric (PGE), City of Gresham, Portland Water Bureau, Columbia Corridor Association (CCA), Zero Waste Alliance (ZWA), Portland Metro, Venture Portland and Portland Office of Sustainable Development.
+              </p>
+
+              <p>
+                The program is designed to benefit the manufacturing and industrial sector in the greater Portland, Oregon metro area. This program fills the needs of under-served manufacturing and industrial sectors.
+              </p>
+
+              <p>
+                <b>
+                  Special Thanks:
+                </b>
+
+                <ul>
+                  <li>
+                    <a href="http://www.oregonmetro.gov/" target="_blank" >
+                      Metro
+                    </a>
+                  </li>
+                  <li>
+                    <a href="http://www.oregon.gov/DEQ/Pages/index.aspx" target="_blank" >
+                      DEQ
+                    </a>
+                  </li>
+                  <li>
+                    <a href="http://www.boeing.com/" target="_blank" >
+                      The Boeing Company
+                    </a>
+                  </li>
+                </ul>
+
+                <div style={{ "display" : "none" , }}>
+                  <img className="ui image small" src="http://res.cloudinary.com/djjldnjz7/image/upload/v1486637792/boeing_logo_iz9hcc.jpg" style={{ "float" : "left" , "margin" : "12px" }}></img>
+
+                  <img className="ui image small" src="http://res.cloudinary.com/djjldnjz7/image/upload/v1486637792/metro_logo_ukb86i.jpg" style={{ "float" : "left" , "margin" : "12px" }}></img>
+
+                  <img className="ui image small" src="http://res.cloudinary.com/djjldnjz7/image/upload/v1486637792/deq_logo_dewlj1.jpg" style={{ "float" : "left" , "margin" : "12px" }}></img>
+                </div>
+              </p>
+
+
+            </div>
+          </div>
         </div>
       )
     }
   });
-
-  const ResourceFullUse = React.createClass({
-
-    updateState(e) {
-      var state = this.state;
-      state[ e.currentTarget.dataset.field ] = e.currentTarget.value
-      this.setState( state );
-    },
-
-    render () {
-      return (
-
-        <div className="ui container" style={{ "color" : "#262262" , "padding" : "30px 20px" ,}}>
-          <h1 className="ui header" style={{ "color" : "#262262"}}>
-            <a href="https://www.facebook.com/groups/ResourceFullUse/" target="_blank" style={{ "color" : "#262262" , }}>
-              ResourceFULL Use
-            </a>
-          </h1>
-
-          <div className="ui grid doubling stackable">
-            <div className="sixteen wide column" >
-              <p>
-                At a 2006 CCA breakfast forum, ResourceFULL Use was born.
-                We never imagined it would be going strong a decade later. Each exchange is money saved, resources utilized, and greenhouse gasses reduced. Our quarterly workshops are a great place to brainstorm and Dzspeed datedz with potential partners.
-              </p>
-
-              <h2>One company’s waste is another’s raw material</h2>
-
-              <p>
-                Industries and manufactures commonly produce wastes that other businesses could use. ResourceFull Use was started to help turn one business’ trash into another’s treasure.
-              </p>
-
-              <p>
-                ResourceFull Use provides manufacturers and industry with workshops to assess resource needs and actively exchange resources (e.g.. use the waste of one organization as an input for another).
-              </p>
-
-              <p>
-                We’ve also teamed with the Materials Exchange Platform to give members expanded capabilities and a broader audience for your by-products. Any materials that you post are now seen by businesses in Oregon and Washington.
-              </p>
-
-              <p>
-                Benefits are improved economic viability through reduced waste, improved resource efficiences, and decreased emissions (CO2 and other greenhouse gases).
-              </p>
-
-              <a href="https://www.facebook.com/groups/ResourceFullUse/" target="_blank" style={{ "color" : "#262262" , }}>
-
-                <img className="ui image large" src="http://res.cloudinary.com/djjldnjz7/image/upload/v1481692448/RESOURCES_RFU_1_03_sgwtw2.jpg" style={{ "float" : "left" , "margin" : "12px" }} / >
-                  <img className="ui image large" src="http://res.cloudinary.com/djjldnjz7/image/upload/v1481692472/RESOURCES_RFU_1_07_zlpojo.jpg" style={{ "float" : "left" , "margin" : "12px" }} />
-                </a>
-              </div>
-
-            </div>
-
-            <div className="ui grid">
-              <div className="sixteen wide column">
-                The ResourceFull Use program is different from traditional passive web-based exchanges in these important ways:
-                <ul style={{"marginBottom":"24px"}}>
-                  <li>It is actively facilitated. Whereas passive exchange databases
-                    rely on the user to identify exchanges, ResourceFull Use does
-                    not rely only on a passive database. Facilitated workshops
-                    and convened business association events provide venues for
-                    organization to actively explore potential exchanges.
-                  </li>
-                  <li>
-                    It looks holistically at all resource flows.
-                    Participating organizations map their resource inputs and outputs
-                    through the whole organization rather than just focusing on
-                    individual waste streams or input needs.
-                  </li>
-                  <li>
-                    It leverages local business associations and government programs. The exchange uses existing programs and relationships to
-                    build awareness and expand the use. It is local. It reduces
-                    transportation costs and promotes community.
-                  </li>
-                  <li>
-                    It is flexible. The program encompasses more than material
-                    resources. The process allows for exchanges involving space,
-                    equipment, energy, and transportation.
-                  </li>
-                </ul>
-
-                <p>
-                  The ResourceFull Use Project started in 2006 as the result of collaboration between Zero WasteAlliance, a Portland nonprofit organization and Columbia Corridor Association. In its first phase the ResourceFull Use program found a home for 250 gallons of paint and reduced 3 tons of greenhouse gasses.
-                </p>
-
-                <p>
-                  The program has benefited from advisory, funding, and in-kind support from a wide cross- section of organizations including Pacific Northwest Pollution Prevention Resource Center, The Boeing Foundation, Cadmus Group (formely Quantec, LLC). Oregon Department of Environmental Quality (DEQ), Portland Development Commission (PDC), City of Portland’s Recycle at Work Program, Portland General Electric (PGE), City of Gresham, Portland Water Bureau, Columbia Corridor Association (CCA), Zero Waste Alliance (ZWA), Portland Metro, Venture Portland and Portland Office of Sustainable Development.
-                </p>
-
-                <p>
-                  The program is designed to benefit the manufacturing and industrial sector in the greater Portland, Oregon metro area. This program fills the needs of under-served manufacturing and industrial sectors.
-                </p>
-
-                <p>
-                  <b>
-                    Special Thanks:
-                  </b>
-
-                  <ul>
-                    <li>
-                      <a href="http://www.oregonmetro.gov/" target="_blank" >
-                        Metro
-                      </a>
-                    </li>
-                    <li>
-                      <a href="http://www.oregon.gov/DEQ/Pages/index.aspx" target="_blank" >
-                        DEQ
-                      </a>
-                    </li>
-                    <li>
-                      <a href="http://www.boeing.com/" target="_blank" >
-                        The Boeing Company
-                      </a>
-                    </li>
-                  </ul>
-
-                  <div style={{ "display" : "none" , }}>
-                    <img className="ui image small" src="http://res.cloudinary.com/djjldnjz7/image/upload/v1486637792/boeing_logo_iz9hcc.jpg" style={{ "float" : "left" , "margin" : "12px" }}></img>
-
-                    <img className="ui image small" src="http://res.cloudinary.com/djjldnjz7/image/upload/v1486637792/metro_logo_ukb86i.jpg" style={{ "float" : "left" , "margin" : "12px" }}></img>
-
-                    <img className="ui image small" src="http://res.cloudinary.com/djjldnjz7/image/upload/v1486637792/deq_logo_dewlj1.jpg" style={{ "float" : "left" , "margin" : "12px" }}></img>
-                  </div>
-                </p>
-
-
-              </div>
-            </div>
-          </div>
-        )
-      }
-    });
