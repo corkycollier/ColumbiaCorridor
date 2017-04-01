5ed0de7a569438353c43eaced0ea6df0f7e7cb09
samullman <samullman@gmail.com>
2017-04-01 15:10:29 -0700

    home page lettering

--- app/assets/javascripts/components/pages/Home.jsx
+++ app/assets/javascripts/components/pages/Home.jsx
@@ -37,6 +37,9 @@ const Home = React.createClass({
       duration   : '0.8s'    ,
     });
 
+
+    $('.shape').shape();
+
     setTimeout(this.flip, 7400)
   },
 
@@ -69,6 +72,8 @@ const Home = React.createClass({
     Backbone.history.navigate(this.state.currentPage.link, { trigger: true })
   },
 
+
+
   flipLeft () {
     var pageNumber = this.state.currentPage.id - 1
     if ( pageNumber < 0 ) pageNumber = 3;
@@ -89,34 +94,44 @@ const Home = React.createClass({
     return (
       <div className="ui-container-sam" style={{"position" : "relative" , "background" : "rgba(63, 63, 63, 0.57)" , }}>
 
-          <div className="ui button circular icon" style={{ "position" : "absolute" , "top" : "30%" , "left" : "16px" , "zIndex" : "200" , "opacity" : "0.6" }} onClick={ this.flipLeft }>
-            <i className="left caret icon"></i>
-          </div>
+        <div className="ui button circular icon" style={{ "position" : "absolute" , "top" : "30%" , "left" : "16px" , "zIndex" : "200" , "opacity" : "0.6" }} onClick={ this.flipLeft }>
+          <i className="left caret icon"></i>
+        </div>
 
-          <div className="ui button circular icon" style={{ "position" : "absolute" , "top" : "30%" , "right" : "16px" , "zIndex" : "200" , "opacity" : "0.6" }} onClick={ this.flipRight }>
-            <i className="right caret icon"></i>
-          </div>
+        <div className="ui button circular icon" style={{ "position" : "absolute" , "top" : "30%" , "right" : "16px" , "zIndex" : "200" , "opacity" : "0.6" }} onClick={ this.flipRight }>
+          <i className="right caret icon"></i>
+        </div>
 
-          <div>
-            <a href={"#" + this.state.currentPage.link}>
-              <img className="ui image centered home-image"src={this.state.currentPage.img} onClick = { this.navigate } style={{
-                  "minHeight" : "282px" ,
-                  "width" : "100vw" ,
-                  "maxWidth" : "1344px" ,
-                }}></img>
-              </a>
-            </div>
+        <div>
+          <a href={"#" + this.state.currentPage.link}>
+
+            <img className="ui image centered home-image" src={this.state.currentPage.img} onClick = { this.navigate } style={{
+                "height" : "505px" ,
+                "minHeight" : "282px" ,
+                "width" : "100vw" ,
+                "maxWidth" : "1344px" ,
+              }}></img>
+            </a>
+          </div>
 
           <div className="ui grid centered stackable " style={{ "background" : "url(http://res.cloudinary.com/djjldnjz7/image/upload/v1481844877/footer_tgdoad.jpg)" , "color" : "#0a0a7a" , "minHeight" : "24vh" , "textAlign" : "center" , "padding" : "15px 0px" ,  "paddingTop" : "19px" , "position" : "relative" , "top" : "14px"}}>
             <div className="five wide column" style = {{ "textAlign" : "center" , "padding" : "0px" }} >
-              <h2><a href="#event-list" style={{ "color" : "#262262" , }}>Upcoming Events</a></h2>
+              <h1><a href="#event-list" style={{
+                  "fontWeight" : "bold" ,
+                  "color" : "#fff" ,
+                  "letterSpacing" : "1px" ,
+                }}>Upcoming Events</a></h1>
               <div>
 
                 {
                   this.props.parent.state.upcoming_events.slice(0, 3).map(function(el) {
                     return (
                       <div key={"fni" + el.id} style={{ "marginTop" : "12px" ,}}>
-                        <a href={"#event/" + el.id} style={{ "color" : "#262262" , }} >
+                        <a href={"#event/" + el.id} style={{
+                            "color" : "#fff" ,
+                            "fontSize" : "16px" ,
+                            
+                          }} >
                           { el.basic_title }
                         </a>
 
@@ -140,7 +155,11 @@ const Home = React.createClass({
             </div>
 
             <div className="five wide column" style = {{ "textAlign" : "center" , "padding" : "0px" }} >
-              <h2><a href="#cca-news" style={{ "color" : "#262262" , }}>News Flash</a></h2>
+              <h1><a href="#cca-news" style={{
+                  "fontWeight" : "bold" ,
+                  "color" : "#fff" ,
+                  "letterSpacing" : "1px" ,
+                }}>News Flash</a></h1>
               <div >
                 {
                   this.props.parent.state.news.slice(0, 3).map(function(el) {
@@ -149,7 +168,10 @@ const Home = React.createClass({
 
                     return(
                       <div key={"fni" + el.id} style={{ "marginTop" : "12px" ,}}>
-                        <a href={"#news/" + el.id} style={{ "color" : "#262262" , }} >
+                        <a href={"#news/" + el.id} style={{
+                            "color" : "#fff" ,
+                            "fontSize" : "16px" ,
+                          }} >
                           { el.basic_title}
                         </a>
 
