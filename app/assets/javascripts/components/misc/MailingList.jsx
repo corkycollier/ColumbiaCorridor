9522f488d42c48ad03099eeb88158cd2da91a934
Randy Bascue <randy@randybascue.com>
2017-04-25 22:10:31 +0000

    attempt to add embed script to end of page

--- app/assets/javascripts/components/misc/MailingList.jsx
+++ app/assets/javascripts/components/misc/MailingList.jsx
@@ -6,6 +6,18 @@ const MailingList = React.createClass({
     this.setState( state );
   },
   
+  componentWillMount() {
+    const script = document.createElement("script");
+    script.src = "https://static.ctctcdn.com/h/contacts-embedded-signup-assets/1.0.2/js/signup-form.js";
+    document.body.appendChild(script);
+    
+    const script2 = document.createElement("script");
+    var postUrlText = document.createTextNode("var postURL = 'https://visitor2.constantcontact.com/api/signup'");
+    script2.appendChild(postUrlText);
+    script2.type="text/javascript";
+    document.body.appendChild(script2);
+    },
+  
   render () {
     return (
       <div class="ctct-embed-signup ui container" style={{ "padding" : "25px" }}>
@@ -26,46 +38,29 @@ const MailingList = React.createClass({
           
           <input data-id="url:input" type="hidden" name="url" value=""></input>
           
-          <div data-id="Email Address:p" class="field">
+          <p data-id="Email Address:p" class="field">
             <label data-id="Email Address:label" data-name="email" class="ctct-form-required">Email Address</label> 
             <input data-id="Email Address:input" type="text" name="email" value="" maxlength="80"></input>
-          </div>
+          </p>
           
-          <div data-id="First Name:p" class="field">
+          <p data-id="First Name:p" class="field">
             <label data-id="First Name:label" data-name="first_name" class="ctct-form-required">First Name</label> 
             <input data-id="First Name:input" type="text" name="first_name" value="" maxlength="50"></input>
-          </div>
+          </p>
           
-          <div data-id="Last Name:p" class="field">
+          <p data-id="Last Name:p" class="field">
             <label data-id="Last Name:label" data-name="last_name" class="ctct-form-required">Last Name</label> 
             <input data-id="Last Name:input" type="text" name="last_name" value="" maxlength="50"></input>
-          </div>
+          </p>
           
-          <div data-id="Company:p" class="field">
+          <p data-id="Company:p" class="field">
             <label data-id="Company:label" data-name="company" class="ctct-form-required">Company</label> 
             <input data-id="Company:input" type="text" name="company" value="" maxlength="50"></input>
-          </div>
+          </p>
           
           <button type="submit" class="ui button" data-enabled="enabled">Sign Up</button>
           
         </form>
-        <script type='text/javascript'>
-           var localizedErrMap = {};
-           localizedErrMap['required'] = 		'This field is required.';
-           localizedErrMap['ca'] = 			'An unexpected error occurred while attempting to send email.';
-           localizedErrMap['email'] = 			'Please enter your email address in name@email.com format.';
-           localizedErrMap['birthday'] = 		'Please enter birthday in MM/DD format.';
-           localizedErrMap['anniversary'] = 	'Please enter anniversary in MM/DD/YYYY format.';
-           localizedErrMap['custom_date'] = 	'Please enter this date in MM/DD/YYYY format.';
-           localizedErrMap['list'] = 			'Please select at least one email list.';
-           localizedErrMap['generic'] = 		'This field is invalid.';
-           localizedErrMap['shared'] = 		'Sorry, we could not complete your sign-up. Please contact us to resolve this.';
-           localizedErrMap['state_mismatch'] = 'Mismatched State/Province and Country.';
-        	 localizedErrMap['state_province'] = 'Select a state/province';
-           localizedErrMap['selectcountry'] = 	'Select a country';
-           var postURL = 'https://visitor2.constantcontact.com/api/signup';
-        </script>
-        <script src="https://static.ctctcdn.com/h/contacts-embedded-signup-assets/1.0.2/js/signup-form.js"></script>
       </div>
     );
   }
