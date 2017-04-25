7f9f3a8f30df8b97c315737388e85a588586b89d
Randy Bascue <randy@randybascue.com>
2017-04-25 21:07:00 +0000

    Remove ajax post request and add script tag variables from embed code

--- app/assets/javascripts/components/misc/MailingList.jsx
+++ app/assets/javascripts/components/misc/MailingList.jsx
@@ -1,17 +1,5 @@
 const MailingList = React.createClass({
 
-  save () {
-    $.ajax({
-      url: 'https://visitor2.constantcontact.com/api/signup',
-      type: 'POST' ,
-      data: {message: this.state} ,
-      success (a, b, c) {
-        alert('Your query was saved.');
-        location.href = "";
-      }
-    });
-  },
-
   update(e) {
     var state = this.state || {};
     state[ e.currentTarget.dataset.field ] = e.currentTarget.value;
@@ -42,6 +30,22 @@ const MailingList = React.createClass({
             </p>
           </div>
         </form>
+        <script type='text/javascript'>
+           var localizedErrMap = {};
+           localizedErrMap['required'] = 		'This field is required.';
+           localizedErrMap['ca'] = 			'An unexpected error occurred while attempting to send email.';
+           localizedErrMap['email'] = 			'Please enter your email address in name@email.com format.';
+           localizedErrMap['birthday'] = 		'Please enter birthday in MM/DD format.';
+           localizedErrMap['anniversary'] = 	'Please enter anniversary in MM/DD/YYYY format.';
+           localizedErrMap['custom_date'] = 	'Please enter this date in MM/DD/YYYY format.';
+           localizedErrMap['list'] = 			'Please select at least one email list.';
+           localizedErrMap['generic'] = 		'This field is invalid.';
+           localizedErrMap['shared'] = 		'Sorry, we could not complete your sign-up. Please contact us to resolve this.';
+           localizedErrMap['state_mismatch'] = 'Mismatched State/Province and Country.';
+        	 localizedErrMap['state_province'] = 'Select a state/province';
+           localizedErrMap['selectcountry'] = 	'Select a country';
+           var postURL = 'https://visitor2.constantcontact.com/api/signup';
+        </script>
         <script src="https://static.ctctcdn.com/h/contacts-embedded-signup-assets/1.0.2/js/signup-form.js"></script>
       </div>
     );
