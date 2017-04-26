942e558d8b87b8cf5b82559a94bcf19777ff00f2
Randy Bascue <randy@randybascue.com>
2017-04-26 03:50:23 +0000

    Add heading and styling to mailing list signup button

--- app/assets/javascripts/components/misc/MailingList.jsx
+++ app/assets/javascripts/components/misc/MailingList.jsx
@@ -8,13 +8,13 @@ const MailingList = React.createClass({
 
   render () {
     return (
-      <div className="ui container">
-        <a href="https://visitor.r20.constantcontact.com/d.jsp?llr=dlpuxoyab&amp;p=oi&amp;m=1125775981923&amp;sit=kizita7kb&amp;f=613e206b-e994-40db-9f1b-f6b30b2b3e8a" className="ui button">
+      <div className="ui container" align="center" style={{ "padding" : "5% 1%" }}>
+        <h1 style={{"marginBottom" : "20px"}}>
+        Join our mailing list!
+        </h1>
+        <a href="https://visitor.r20.constantcontact.com/d.jsp?llr=dlpuxoyab&amp;p=oi&amp;m=1125775981923&amp;sit=kizita7kb&amp;f=613e206b-e994-40db-9f1b-f6b30b2b3e8a" className="ui button" style={{ "fontSize":"1.5em"}}>
           Sign Up Now!
         </a>
-        <div id="ctct_button_footer">
-          For Email Newsletters you can trust.
-        </div>
       </div>
     );
   }
