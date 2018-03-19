c6b710b3a96c481f54b8da2de7f1e4033047b25f
Mykl Clason <myklclason@gmail.com>
2018-03-19 23:43:28 +0000

    Don't store nil values for board members

--- app/models/board.rb
+++ app/models/board.rb
@@ -10,6 +10,19 @@
 #
 
 class Board < ActiveRecord::Base
+  
+  before_save :standardize
+  
+  def standardize
+    # nil values cause errors so don't store nil values
+    self.title   = "" unless title.present?
+    self.name    = "" unless name.present?
+    self.email   = "" unless email.present?
+    self.company = "" unless company.present?
+    self.website = "" unless website.present?
+    self.img     = "" unless img.present?
+    self.bio     = "" unless bio.present?
+  end
 
   def safe_show
     self.attributes
