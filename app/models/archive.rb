ba24f323592e8bed745886649891271552077e5a
Mykl Clason <myklclason@gmail.com>
2018-03-26 22:41:48 +0000

    Add year in review data generation to archive model

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -11,6 +11,20 @@
 
 class Archive < ActiveRecord::Base
 
+  def self.year_in_review
+    fallback_year = '2017'
+    fallback_url = '/pdfjs/minimal?file=%2Fpdf%2Fyear-in-review.pdf'
+
+    archive = Archive.where(title: "Year in Review", doc_type: "file pdf outline")
+                     .reorder(year: :desc)
+                     .first
+
+    {
+      year: archive.try(:year) || fallback_year,
+      url: archive.try(:url) || fallback_url,
+    }
+  end
+
   def safe_show
     self.attributes
   end
