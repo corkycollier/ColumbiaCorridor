1b68f19016fa36dba6bc4dda95ffd97bc94e282b
Mykl Clason <myklclasonwebapps@gmail.com>
2019-07-21 19:04:51 +0000

    Add Guidestar Seal

--- app/assets/javascripts/components/pages/AboutCCA.jsx
+++ app/assets/javascripts/components/pages/AboutCCA.jsx
@@ -26,6 +26,16 @@ const AboutCCA = React.createClass({
               The Columbia Corridor is the single largest economic corridor in Oregon, stretching from the Willamette River in the west to the Sandy River in the east. It’s the largest industrial area in the state, with 2,500 businesses employing 65,000 people with more middle wage jobs than anywhere else in the state. 40% of those employees live within five miles of work.
             </p>
 
+            <br/>
+
+            <div className='ui grid centered'>
+              <a href="https://www.guidestar.org/profile/93-0930033" target="_blank">
+                <img src="https://widgets.guidestar.org/gximage2?o=8389378&l=v4" />
+              </a>
+            </div>
+
+            <br/>
+
             <p>
               The Columbia Corridor is a dynamic mix of manufacturing, transportation, export facilities and even a few small neighborhoods. Our members include the Oregon Humane Society (located in the Corridor for the past century), the Oregon Food Bank and Mt. Hood Community College. A full third of the Columbia Corridor is open space. The Columbia Slough runs 18 miles down the middle of the Corridor, from Fairview and Blue Lakes to Kelly Point Park. We’re home to Smith & Bybee Lakes, Vanport Wetlands, west Hayden Island, and Delta Park.
             </p>
